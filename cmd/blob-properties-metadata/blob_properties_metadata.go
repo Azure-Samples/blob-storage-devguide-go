016dce8b0d1c8b2d2f13219ce8e71e90aee7f99a
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2024-04-05 14:22:28 -0400

    Add properties and metadata samples

--- cmd/blob-properties-metadata/blob_properties_metadata.go
+++ cmd/blob-properties-metadata/blob_properties_metadata.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blob"
@@ -21,10 +23,17 @@ func setBlobProperties(client azblob.Client, containerName string, blobName stri
 	// Reference the blob as a client object
 	blobClient := client.ServiceClient().NewContainerClient(containerName).NewBlobClient(blobName)
 
-	// Set the blob properties
-	contentType := "text/plain"
-	_, err := blobClient.SetHTTPHeaders(context.TODO(), blob.HTTPHeaders{
-		BlobContentType: &contentType,
+	// Get the existing blob properties
+	resp, err := blobClient.GetProperties(context.TODO(), nil)
+	handleError(err)
+
+	// Set the new blob properties and include existing properties
+	_, err = blobClient.SetHTTPHeaders(context.TODO(), blob.HTTPHeaders{
+		BlobContentType:        to.Ptr("text/plain"),
+		BlobContentLanguage:    to.Ptr("en-us"),
+		BlobContentEncoding:    resp.ContentEncoding,
+		BlobContentDisposition: resp.ContentDisposition,
+		BlobCacheControl:       resp.CacheControl,
 	}, nil)
 	handleError(err)
 }
@@ -38,15 +47,35 @@ func getBlobProperties(client azblob.Client, containerName string, blobName stri
 	handleError(err)
 
 	// Print the blob properties
-	log.Printf("Blob content type: %s", *resp.ContentType)
+	fmt.Printf("Content type: %v\n", *resp.ContentType)
+	fmt.Printf("Content language: %v\n", *resp.ContentLanguage)
 }
 
 func setBlobMetadata(client azblob.Client, containerName string, blobName string) {
+	// Reference the blob as a client object
+	blobClient := client.ServiceClient().NewContainerClient(containerName).NewBlobClient(blobName)
 
+	// Set the blob metadata
+	var metadata = make(map[string]*string)
+	metadata["type"] = to.Ptr("image")
+	metadata["category"] = to.Ptr("high-res")
+
+	_, err := blobClient.SetMetadata(context.TODO(), metadata, nil)
+	handleError(err)
 }
 
 func getBlobMetadata(client azblob.Client, containerName string, blobName string) {
+	// Reference the blob as a client object
+	blobClient := client.ServiceClient().NewContainerClient(containerName).NewBlobClient(blobName)
 
+	// Get the blob properties, which includes metadata
+	resp, err := blobClient.GetProperties(context.TODO(), nil)
+	handleError(err)
+
+	// Print the blob metadata
+	for k, v := range resp.Metadata {
+		fmt.Printf("%v: %v\n", k, *v)
+	}
 }
 
 func main() {
---
+++ cmd/container-properties-metadata/container-properties-metadata.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+)
+
+// Blob dev guide properties/metadata sample
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func getContainerProperties(client azblob.Client, containerName string) {
+	// Reference the container as a client object
+	containerClient := client.ServiceClient().NewContainerClient(containerName)
+
+	// Get the container properties
+	resp, err := containerClient.GetProperties(context.TODO(), nil)
+	handleError(err)
+
+	// Print the container properties
+	fmt.Printf("Blob public access: %v\n", *resp.BlobPublicAccess)
+	fmt.Printf("Lease status: %v\n", *resp.LeaseStatus)
+	fmt.Printf("Lease state: %v\n", *resp.LeaseState)
+	fmt.Printf("Has immutability policy: %v\n", *resp.HasImmutabilityPolicy)
+}
+
+func setContainerMetadata(client azblob.Client, containerName string) {
+	// Reference the container as a client object
+	containerClient := client.ServiceClient().NewContainerClient(containerName)
+
+	// Set the container metadata
+	var metadata = make(map[string]*string)
+	metadata["key1"] = to.Ptr("value1")
+	metadata["key2"] = to.Ptr("value2")
+
+	_, err := containerClient.SetMetadata(context.TODO(), nil)
+	handleError(err)
+}
+
+func getContainerMetadata(client azblob.Client, containerName string) {
+	// Reference the container as a client object
+	containerClient := client.ServiceClient().NewContainerClient(containerName)
+
+	// Get the blob properties, which includes metadata
+	resp, err := containerClient.GetProperties(context.TODO(), nil)
+	handleError(err)
+
+	// Print the blob metadata
+	for k, v := range resp.Metadata {
+		fmt.Printf("%v: %v\n", k, *v)
+	}
+}
+
+func main() {
+	// TODO: replace <storage-account-name> with your actual storage account name
+	url := "https://<storage-account-name>.blob.core.windows.net/"
+
+	credential, err := azidentity.NewDefaultAzureCredential(nil)
+	handleError(err)
+
+	client, err := azblob.NewClient(url, credential, nil)
+	handleError(err)
+
+	containerName := "sample-container"
+
+	getContainerProperties(*client, containerName)
+	setContainerMetadata(*client, containerName)
+	getContainerMetadata(*client, containerName)
+}
